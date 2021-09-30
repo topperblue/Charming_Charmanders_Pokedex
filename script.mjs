a6877580b9f78914a33fbe7826dd7356ec6707bc
karis <75146885+KarisWang@users.noreply.github.com>
2021-09-30 19:56:37 -0400

    add basic search function

--- script.mjs
+++ script.mjs
@@ -1,13 +1,27 @@
-import fetch from "node-fetch"
+// import fetch from "node-fetch"
 
 
-for (let i = 1; i <= 150; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then(res => res.json())
-      .then(pokemon => {
-        console.log(pokemon);
-      });
+// for (let i = 1; i <= 150; i++) {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(pokemon => {
+//         console.log(pokemon);
+//       });
+// }
+
+const result = document.getElementById('result');
+const form = document.getElementById('form');
+const userSearch = document.getElementById('query');
+var baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+async function searchPokeAPI(event) {
+  event.preventDefault();
+  const pokeUrl = baseUrl + userSearch.value + "/";
+  const data = await fetch(pokeUrl);
+  const json = await data.json();
+  result.innerHTML = json.name;
+  console.log(json.name);
 }
 
-
+form.addEventListener('submit', searchPokeAPI);
