24818f3631ea48941eb4b000bd1cd7dba1c9c043
Adam Gerig <gerigadam2000@gmail.com>
2021-10-13 19:01:35 -0400

    Added search functionality for uppercase

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ SEARCH
 ***********************************/
 async function searchPokeAPI(event) {
   event.preventDefault();
-  pokeUrl = baseUrl + userSearch.value + "/";
+  pokeUrl = baseUrl + userSearch.value.toLowerCase() + "/";
   try {
     fetch(pokeUrl)
     const data = await fetch(pokeUrl);
